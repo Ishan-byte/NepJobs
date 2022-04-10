e60567f667624e6ed62c74dc4dd981b28588723d
SamratGhale <Samrat.biz19@gmail.com>
2022-04-11 00:36:21 +0545

    new nav and drawer

--- frontend/src/Layout/index.js
+++ frontend/src/Layout/index.js
@@ -1,10 +1,187 @@
-import NavBar from "../nav";
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Button } from '@material-ui/core';
+import Navconfig from '../nav/config';
+const drawerWidth = 240;
 
-export default function DashboardLayout({children}) {
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+function navItem({ navs, open }) {
+    return (navs.map((item, i) => {
+        return (<ListItemButton
+            key={i}
+            onClick={(e) => {
+                e.preventDefault();
+                window.location = item.href
+            }}
+            sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+            }}
+        >
+            <ListItemIcon
+                sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                }}
+            >
+                {item.icon}
+            </ListItemIcon>
+            <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0 }} />
+        </ListItemButton>
+        )
+    }))
+}
+
+export default function DashboardLayout({ children }) {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
     return (
-        <div>
-            <NavBar />
-            <div>{children}</div>
-        </div>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Nep Jobs
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {Navconfig.map((navList, i) => {
+                        const Guard = navList.guard;
+                        const authUsers = navList.roles || [];
+                        return (
+                            <Guard key={i} authUsers={authUsers}>
+                                {open ? (
+                                    <Typography variant='button' gutterBottom  >
+                                       {  navList.title}
+                                    </Typography>
+                                )
+                                    : ""}
+                                {navItem({ navs: navList.navs, open })}
+                            </Guard>
+                        )
+                    }
+                    )}
+                </List>
+                <Divider />
+                <List>
+                </List>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+
+                <div>{children}</div>
+            </Box>
+        </Box>
     )
 }
--- frontend/src/nav/config.js
+++ frontend/src/nav/config.js
@@ -1,6 +1,10 @@
 import { ROLES } from "../Constants";
 import { pagePath, routes } from "../Routes/path";
 import AuthProtectNav from "./AuthProtectNav";
+import HomeIcon from '@material-ui/icons/Home';
+import WorkIcon from '@material-ui/icons/Work';
+import RecentActorsIcon from '@material-ui/icons/RecentActors';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 
 const Navconfig = [
   //For admin user
@@ -8,35 +12,35 @@ const Navconfig = [
   //For normal user
   {
     guard: AuthProtectNav,
-    roles: [ROLES.ADMIN, ROLES.EMPLOYEER],
+    roles: [ROLES.ADMIN, ROLES.EMPLOYEER, ROLES.JOBSEEKER],
+    title: "App",
     navs: [
       {
         title: "Home",
         href: routes.app,
+        icon: <HomeIcon/>
       },
-    ],
-  },
-  {
-    guard: AuthProtectNav,
-    roles: [ROLES.ADMIN, ROLES.EMPLOYEER, ROLES.JOBSEEKER],
-    navs: [
       {
         title: "Jobs",
         href: pagePath.app.jobs,
+        icon: <WorkIcon/>
       },
     ],
   },
   {
     guard: AuthProtectNav,
     roles: [ROLES.ADMIN],
+    title: "Admin",
     navs: [
       {
         title: "Admin",
         href: routes.admin,
+        icon: <PeopleAltIcon/>
       },
       {
         title: "Users",
         href: pagePath.admin.users,
+        icon: <RecentActorsIcon/>
       },
     ],
   },
