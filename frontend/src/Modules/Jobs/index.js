0c1ee985ab5f7a493b0ef3f22f2d7d4c985877e9
SamratGhale <Samrat.biz19@gmail.com>
2022-04-11 21:18:09 +0545

    added search on jobs

--- frontend/src/Modules/Jobs/index.js
+++ frontend/src/Modules/Jobs/index.js
@@ -1,5 +1,10 @@
 import React, { useState, useContext, useEffect } from "react";
 import clsx from 'clsx';
+import DetailsIcon from '@material-ui/icons/Details';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import { TextField } from "@mui/material";
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
 
 import { SnackbarProvider, useSnackbar } from "notistack";
 import Grid from '@material-ui/core/Grid';
@@ -20,12 +25,16 @@ import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { colors } from "@material-ui/core";
 
+function escapeRegExp(value) {
+    return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
+
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     paper: {
-        height: 140,
+        height: 190,
         width: 300,
         elevation: 3,
         variant: "outlined",
@@ -39,8 +48,65 @@ const useStyles = makeStyles((theme) => ({
     },
     card:{
         backgroundColor: colors.teal[200],
-    }
+    },
+    textField: {
+        [theme.breakpoints.down('xs')]: {
+          width: '100%',
+        },
+        margin: theme.spacing(1, 0.5, 1.5),
+        '& .MuiSvgIcon-root': {
+          marginRight: theme.spacing(0.5),
+        },
+        '& .MuiInput-underline:before': {
+          borderBottom: `1px solid ${theme.palette.divider}`,
+        },
+      },
 }));
+
+
+
+function QuickSearchToolbar({jobs, setCurrJobs}) {
+    
+    const classes = useStyles();
+    const [searchText, setSearchText] = useState('');
+
+    const requestSearch = (searchValue) => {
+        setSearchText(searchValue);
+        const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
+        const filteredRows = jobs.filter((row) => {
+            return Object.keys(row).some((field) => {
+                return searchRegex.test(row[field].toString());
+            });
+        });
+        setCurrJobs(filteredRows);
+    };
+
+    return (
+        <div >
+            <TextField
+                variant="standard"
+                value={searchText}
+                onChange={(e)=>requestSearch(e.target.value)}
+                placeholder="Search…"
+                className={classes.textField}
+                InputProps={{
+                    startAdornment: <SearchIcon fontSize="medium" />,
+                    endAdornment: (
+                        <IconButton
+                            title="Clear"
+                            aria-label="Clear"
+                            size="small"
+                            style={{ visibility: {searchText} ? 'visible' : 'hidden' }}
+                        >
+                            <ClearIcon onClick={(e)=>requestSearch('')} fontSize="medium" />
+                            
+                        </IconButton>
+                    ),
+                }}
+            />
+        </div>
+    );
+}
 
 const JobSingle = ({ job }) => {
     const classes = useStyles();
@@ -53,19 +119,21 @@ const JobSingle = ({ job }) => {
                 />
                 <CardContent>
                     {job.requirements.map((req, i) => {
-                        return (
-                        <Typography key={i} variant="body1" >
-                            -  {req}
-                        </Typography>
-                        )
+                        if (i < 2) {
+                            return (
+                                <Typography key={i} variant="body1" >
+                                    -  {req}
+                                </Typography>
+                            )
+                        }
                     })}
                 </CardContent>
                 <CardActions disableSpacing>
                     <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
+                        <BookmarkIcon/>
                     </IconButton>
                     <IconButton aria-label="share">
-                        <ShareIcon />
+                        <DetailsIcon/>
                     </IconButton>
                 </CardActions>
             </Card>
@@ -78,21 +146,35 @@ const JobsComponent = () => {
 
     const classes = useStyles();
 
+
     const { jobs, refreshData, refresh } = useContext(JobsContext);
     const [currJobs, setCurrJobs] = useState([]);
+
+    useEffect(async()=>{
+        await refreshData()
+        setCurrJobs(jobs);
+    },[jobs])
+
 
     useEffect(() => {
         refreshData();
     }, [refresh])
     return (
         <div className={classes.root}  >
-            <Typography variant="h2">
-                Jobs
-            </Typography>
+            <Grid container className="col-lg-8">
+                <Grid item className="col-lg-5">
+                    <Typography variant="h2">
+                        Jobs
+                    </Typography>
+                </Grid>
+                <Grid item className="col-lg-3">
+                    <QuickSearchToolbar setCurrJobs={setCurrJobs} jobs={jobs} />
+                </Grid>
+            </Grid>
             <Grid container className={classes.root} spacing={4}>
                 <Grid item xs={12}>
                     <Grid container justifyContent="center" spacing={9}>
-                        {jobs.map((job) => (
+                        {currJobs.map((job) => (
                             <Grid key={job._id} item>
                                 <JobSingle job={job} />
                             </Grid>
